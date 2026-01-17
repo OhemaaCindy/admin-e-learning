133502c5c91ce3df931d43ffbc1b2c346bd7e10c
OhemaaCindy <cindyessuman67@gmail.com>
2026-01-17 12:04:27 +0000

    changed the logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,7 +1,7 @@
 export const Logo: React.FC = () => {
   return (
     <div className="flex justify-center mb-6 items-center">
-      <img src="/images/logo.png" alt="logo" />
+      <img src="/images/logo.png" alt="logo"  className="w-50 h-10"/>
     </div>
   );
 };
